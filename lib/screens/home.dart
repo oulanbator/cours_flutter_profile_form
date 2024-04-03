2703d307b47f168b6704eca1642f18b148e27dc9
AlbrizioLuca <luca_albrizio@hotmail.com>
2024-04-03 16:03:42 +0200

    test push

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -39,7 +39,7 @@ class Home extends StatelessWidget {
         onPressed: () => Navigator.push(
           context,
           MaterialPageRoute(
-            builder: (context) => ProfilCreate(),
+            builder: (context) => const ProfilCreate(),
           ),
         ),
         tooltip: 'Nouveau profil',
